432d527dc03584a6592e07ff848888c8679b0b93
Edison Lim <117683125+EdisonLim17@users.noreply.github.com>
2025-06-27 19:04:43 -0400

    Successfully called and tested API, but test is not complete

--- test_api.py
+++ test_api.py
@@ -1,7 +1,30 @@
-import re
-from playwright.sync_api import Page, expect
+import re, pytest
+from typing import Generator
+from playwright.sync_api import Playwright, APIRequestContext, Page, expect
 
-def test_has_title1(page: Page):
-    page.goto("https://edisonlim.ca")
+BASE_URL = "https://5ak2nip9i9.execute-api.us-east-1.amazonaws.com/"
 
-    expect(page).to_have_title("Edison Lim - Portfolio")
+# def test_has_title(page: Page):
+#     page.goto("https://edisonlim.ca")
+
+#     expect(page).to_have_title("Edison Lim - Portfolio")
+
+@pytest.fixture(scope="session")
+def api_request_context(playwright: Playwright) -> Generator[APIRequestContext, None, None]:
+    headers = {
+        "method": "POST"
+    }
+    request_context = playwright.request.new_context(
+        base_url=BASE_URL, extra_http_headers=headers
+    )
+    yield request_context
+    request_context.dispose()
+
+def test_updates_database(api_request_context: APIRequestContext) -> None:
+    data = {}
+
+    response = api_request_context.post("/prod/websitecounterlambdaendpoint", data=data)
+
+    assert response.status == 200
+    assert response.ok
+    assert response.json()["num_views"] == 141
