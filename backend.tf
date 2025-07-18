3f1f95e8cf13c6f7ff1f7a609db5b09bda563e44
Edison Lim <117683125+EdisonLim17@users.noreply.github.com>
2025-07-18 18:31:28 -0400

    Fixed key-path for backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket       = "personal-website-backend-tf-state"
-    key          = "personal-website-backend/terraform.tfstate"
+    key          = "terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = true
     encrypt      = true
