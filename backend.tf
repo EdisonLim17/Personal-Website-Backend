993be4bf9edf23bbf80cc32a1ca918b6418830a9
Edison Lim <117683125+EdisonLim17@users.noreply.github.com>
2025-07-18 18:23:34 -0400

    Fixed Terraform format for backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "personal-website-backend-tf-state"
-    key            = "personal-website-backend/terraform.tfstate"
-    region         = "us-east-1"
-    use_lockfile   = true
-    encrypt        = true
+    bucket       = "personal-website-backend-tf-state"
+    key          = "personal-website-backend/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+    encrypt      = true
   }
 }
